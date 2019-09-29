4a994606ecb16d5778919b824ba4a86ddd8b26b2
free-zhang <43287556+free-zhang@users.noreply.github.com>
2019-09-29 11:01:05 +0800

    add comment 支持类型
    add comment 支持类型

--- debug.h
+++ debug.h
@@ -1,8 +1,9 @@
 /* Created by freezhang on 2019/9/29.
  * @初衷：python的print函数可以非常方便的打印变量的内容，调试起来很方便。在c++中没有此方法，于是写了PRINT(a)模拟一下，此宏比较方便的地方是可以打印出变量的名字
  * @类debug: 约定T1, T2,T3为c++基础类型以及string类型，Debug类定义的str方法可以将 T1, vector<T1>, vector<vector<T1>, map<T1, T1>, map<T1, map<T2, T3>>, set<T1>转换为string
- * @ 使用方法：PRINT(a) 宏可以将 变量所在的行，变量的名字，变量值打印出
- *            SHOW_VAR 宏可以控制是否打印变量
+ * @使用方法：PRINT(a) 宏可以将 变量所在的行，变量的名字，变量值打印出
+ *           SHOW_VAR 宏可以控制是否打印变量
+ * @支持类型： 约定T1, T2,T3为c++基础类型以及string类型，目前支持 T1, vector<T1>, vector<vector<T1>, map<T1, T1>, map<T1, map<T2, T3>>, set<T1>
  */
 
 #ifndef CLION_TEST_DEBUG_H
